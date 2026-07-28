c80adf7a5192b2b49bb3217ee8345599d670b0a5
KevinSteele2 <kevin.steele@outlook.com>
2026-07-27 17:05:56 -0700

    Delete exploration.py

--- exploration.py
+++
@@ -1,9 +0,0 @@
-import requests
-import pandas as pd
-import matplotlib.pyplot as plt
-import numpy as np
-import statsmodels.formula.api as smf
-
-panel_long["resid"] = model.resid
-outliers = panel_long.reindex(panel_long["resid"].abs().sort_values(ascending=False).index)
-print(outliers[["date", "group", "total_traffic", "resid"]].head(15))
